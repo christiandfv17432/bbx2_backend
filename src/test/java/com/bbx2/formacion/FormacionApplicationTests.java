c3c7b85f352774612cf845697b51a8e5726eb47a
CHRISTIAN DE LA FRENZA VELOZ <christian.frenza@ikeasi.com>
2021-03-08 12:44:53 +0000

    Creacion del proyecto

---
+++ src/test/java/com/bbx2/formacion/FormacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbx2.formacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
