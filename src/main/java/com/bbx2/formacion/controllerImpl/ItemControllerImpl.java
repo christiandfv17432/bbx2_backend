81002447778820570acc82378b96093c0d527d2c
Christian <christiandfv17432@gmail.com>
2021-03-11 11:21:21 +0000

    Creación del controller para peticiones REST

--- src/main/java/com/bbx2/formacion/controllerImpl/ItemControllerImpl.java
+++ src/main/java/com/bbx2/formacion/controllerImpl/ItemControllerImpl.java
@@ -4,9 +4,7 @@ import com.bbx2.formacion.controller.ItemController;
 import com.bbx2.formacion.dto.ItemDTO;
 import com.bbx2.formacion.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,20 @@ public class ItemControllerImpl implements ItemController {
     @GetMapping("/findAllItems")
     public List<ItemDTO> getAllItems() {
         return itemService.findAllItems();
+    }
+
+    @PostMapping("/saveItem")
+    public void createItem(ItemDTO itemDTO) {
+        itemService.save(itemDTO);
+    }
+
+    @DeleteMapping("/deleteItem/{id}")
+    public void deleteItem(@RequestParam(name = "id",required = true) Long id) {
+        itemService.deleteItem(id);
+    }
+
+    @GetMapping("/getItemById/{id}")
+    public ItemDTO getItem(@RequestParam(name = "id",required = true) Long id) {
+        return itemService.getItem(id);
     }
 }
